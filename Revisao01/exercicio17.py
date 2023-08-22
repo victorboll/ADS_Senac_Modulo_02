7369c065e77575ad1346a6ad7eaa6d189e37f539
Victor Bollentini <victorboll@yahoo.com.br>
2023-08-22 17:00:34 -0300

    Arquivo 17 corrigido para atender ao que foi solicitado no enunciado

--- Revisao01/exercicio17.py
+++ Revisao01/exercicio17.py
@@ -20,23 +20,33 @@ litros = area/6
 galao = math.ceil(litros/18)
 
 print(f'A quantidade de latas necessárias para pintar uma área de {area} metros quadrados, seria {int(galao)} '
-      f'latas, e o preço total será {(galao*80):.2f}.')
+      f'galao, e o preço total será {(galao*80):.2f}.')
 
 lata = math.ceil(litros/3.6)
 
 print(f'A quantidade de latas necessárias para pintar uma área de {area} metros quadrados, seria {int(lata)} '
-      f'latas, e o preço total será {(lata*80):.2f}.')
+      f'latas, e o preço total será {(lata*25):.2f}.')
 
 galao, lata = 0, 0
+litros *= 1.1
 if litros >= 18:
-    galao = math.ceil(litros/18)
-    litros = round((litros - galao*18),13) #Quando não dava round(), quando ele diminuia 3.6 de 10.8, estava dando
-    # 7.200000000000001, o que dava um erro na conta
+    galao = litros//18
+    litros = round((litros - galao*18), 13)
+    """Quando não dava round(), quando ele diminuia 3.6 de 10.8, estava dando
+    7.200000000000001, o que dava um erro na conta"""
     print(litros)
 if litros > 10.8:
-    galao = math.ceil(litros / 18)
-    litros = round((litros - galao * 18), 13)
-if litros > 0:
+    galao += 1
+elif litros > 0:
     lata = math.ceil(litros / 3.6)
 
-print(f'galoes {galao} /// lata {lata}  //// litros {litros}')
+if galao > 0 and lata > 0:
+    print(f'Tendo em vista o melhor custo, para pintar uma área de {area} metros quadrados, '
+          f'será necessário {int(galao)} galoes e {int(lata)} latas, o preço total será '
+          f'R${(lata * 25 + galao * 80):.2f} (R${(lata * 25):.2f} pelas latas e R${(galao * 80):.2f} pelos galoes.')
+elif galao > 0 and lata == 0:
+    print(f'Tendo em vista o melhor custo, para pintar uma área de {area} metros quadrados, '
+          f'será necessário {int(galao)} galoes, o preço total será R${(galao * 80):.2f}.')
+else:
+    print(f'Tendo em vista o melhor custo, para pintar uma área de {area} metros quadrados, '
+          f'será necessário {int(lata)} latas, o preço total será R${(lata * 25):.2f}.')
