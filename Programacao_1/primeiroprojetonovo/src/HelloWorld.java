6820762299228c45bc7db486700f5340d390bbf3
DanielaSegadilhae <36129382023.1N@senacrjedu.local>
2023-08-25 19:54:57 -0300

    Adição da pasta de programação 1

---
+++ Programacao_1/primeiroprojetonovo/src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+*
+*@author 36129382023.1N
+*
+*/
+
+
+public class HelloWorld {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+	}
+}
---
+++ Programacao_1/variaveis/src/TesteAtribuicaoVariaves1.java
@@ -0,0 +1,38 @@
+
+public class TesteAtribuicaoVariaves1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Definição de variáveis
+		boolean g;
+		char c;
+		byte b;
+		short s;
+		int i;
+		long l;
+		float f;
+		double d;
+		
+		// Atribuição de variáveis
+		g = true;
+		c = '1';
+		b = 0;
+		s = 0;
+		i = 0;
+		l = 0;
+		f = 0;
+		d = 0;
+		
+		// Exibição de variáveis
+		System.out.println("g="+ g);
+		System.out.println("c="+ c);
+		System.out.println("b="+ b);
+		System.out.println("s="+ s);
+		System.out.println("i="+ i);
+		System.out.println("l="+ l);
+		System.out.println("f="+ f);
+		System.out.println("d="+ d);
+	}
+
+}
