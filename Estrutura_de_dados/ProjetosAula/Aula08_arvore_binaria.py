13af6a51cab20b1e1677de179d2a342f482601b5
Victor <victorboll@yahoo.com.br>
2023-11-14 21:34:46 -0300

    Arvore em estrutura de dados

--- Estrutura_de_dados/ProjetosAula/Aula08_arvore_binaria.py
+++ Estrutura_de_dados/ProjetosAula/Aula08_arvore_binaria.py
@@ -16,7 +16,7 @@ raiz.direita = NodoArvore(1)
 print("Árvore: ", raiz)
 
 def em_ordem(raiz):
-    if not raiz:
+    if not raiz: #Para quando for None, ele não faz as ações, ele termina a recursão se o "ramo" deu none.
         return
 
     #Visita filho da esquerda.
@@ -40,3 +40,66 @@ raiz2.esquerda.esquerda = NodoArvore(10)
 raiz2.esquerda.direita = NodoArvore(30)
 
 em_ordem(raiz2)
+
+print("Árvore: ", raiz2)
+print("Árvore: ", raiz2.direita)
+print("Árvore: ", raiz2.esquerda)
+print("Árvore: ", raiz2.esquerda.esquerda)
+
+
+def pre_ordem(raiz):
+    if not raiz:
+        return
+
+    print(raiz.chave)
+
+    pre_ordem(raiz.esquerda)
+
+    pre_ordem(raiz.direita)
+
+
+def pos_ordem(raiz):
+    if not raiz:
+        return
+
+    pos_ordem(raiz.esquerda)
+
+    pos_ordem(raiz.direita)
+
+    print(raiz.chave)
+
+print()
+pre_ordem(raiz2)
+print()
+pos_ordem(raiz2)
+
+
+def insere(raiz, nodo):
+    """Insere um nodo em uma árvore binária de pesquisa."""
+    #Nodo deve ser inserido na raiz.
+    if raiz is None:
+        raiz = nodo
+
+    #Nodo deve ser inserido na subárvore direita
+    elif raiz.chave < nodo.chave:
+        if raiz.direita is None:
+            raiz.direita = nodo
+        else:
+            insere(raiz.direita, nodo)
+
+    #Nodo deve ser inserido na subárvore esquerda
+    else:
+        if raiz.esquerda is None:
+            raiz.esquerda = nodo
+        else:
+            insere(raiz.esquerda, nodo)
+
+
+array = (1, 3, 7, 0, 5, 2, 4, 9, 8)
+raiz3 = NodoArvore(-1)
+for i in array:
+    nodo = NodoArvore(i)
+    insere(raiz3, nodo)
+
+em_ordem(raiz3)
+print(raiz3)
